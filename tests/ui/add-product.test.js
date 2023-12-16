6da049feb76ba597f34d0fcbba600f021fd45a5e
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2023-12-16 15:02:47 +0200

    Change the URL in the UI tests to a deployed application on Render.com

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-Product');
+    await page.goto('https://kuzmanovb-exam.onrender.com/add-product');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
